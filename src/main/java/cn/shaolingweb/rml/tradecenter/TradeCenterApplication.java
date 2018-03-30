54f65ea4bd1410f3bf6ede7734a95388b44ea821
shaoling <shaoling@jd.com>
2018-03-30 10:53:14 +0800

    使用JDK8

--- src/main/java/cn/shaolingweb/rml/tradecenter/TradeCenterApplication.java
+++ src/main/java/cn/shaolingweb/rml/tradecenter/TradeCenterApplication.java
@@ -1,6 +1,7 @@
 package cn.shaolingweb.rml.tradecenter;
 
 import cn.shaolingweb.rml.tradecenter.domain.Hq;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +10,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableScheduling
+@Slf4j
 public class TradeCenterApplication {
 
     public static void main(String[] args) {
+        log.info("启动应用...");
         SpringApplication.run(TradeCenterApplication.class, args);
+        log.info("启动应用完成.");
     }
 }
